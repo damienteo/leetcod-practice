1250ce35c6e4fe03280c97996fe138bbc1162ff9
damienteo <damien.teo.ks@gmail.com>
2025-04-22 16:17:31 +0800

    Add solution for TwoSum

---
+++ TwoSum.ts
@@ -0,0 +1,65 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+// Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+function twoSum(nums: number[], target: number): number[] {
+  // brute force
+
+  //   for (let i = 0; i < nums.length; i++) {
+  //     for (let j = i + 1; j < nums.length; j++) {
+  //       if (nums[i] + nums[j] === target) {
+  //         return [i, j];
+  //       }
+  //     }
+  //   }
+
+  // with map
+
+  const map = new Map<number, number>();
+  // the second num is the index in the array
+  // the first num is the value of the element based on the index
+
+  for (let i = 0; i < nums.length; i++) {
+    // map is initially empty
+    // while going through the loop,
+    // we save invalid values in the map,
+    // for reference later
+    const nextNum = target - nums[i];
+
+    if (map.has(nextNum) && map.get(nextNum) !== undefined) {
+      return [map.get(nextNum) as number, i];
+    }
+
+    map.set(nums[i], i);
+  }
+
+  throw new Error("No two sum solution");
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+console.log(twoSum([3, 2, 4], 6)); // [1, 2]
